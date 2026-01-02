7ad006e891420a0c07dd939f24773f42e4902c0e
bartoszb0 <bartoszb2020@gmail.com>
2026-01-02 18:56:19 +0100

    updated description

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,10 +4,9 @@ export default function AboutMe() {
       <div>
         <div id="name">Bartosz Bednarczyk</div>
         <div id="description">
-          Frontend-focused developer specializing in React and TypeScript, with
-          solid full-stack experience using Django. I independently build and
-          deploy high-quality web applications, turning complex ideas into
-          polished, user-centric interfaces and functional code.
+          Frontend developer specializing in React and TypeScript. I build
+          polished, user-focused web applications and have full-stack experience
+          with Django for end-to-end development.
         </div>
         <div id="contactLinks">
           <a
